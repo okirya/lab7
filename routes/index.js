2e1bd7219655ce17a03d4527044c3d6d1619ee7c
Rotimi Kirya <okirya@ucsd.edu>
2020-02-20 12:59:31 -0800

    Lab 7 Push

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,16 @@ var projects = require('../projects.json');
  * GET home page.
  */
 
+exports.viewAlt = function(request, response){
+	for(i in projects.projects){
+		projects.projects[i]["viewAlt"] = true;
+	}
+  	response.render('index', projects);
+};
+
 exports.view = function(request, response){
+	for(i in projects.projects){
+		projects.projects[i]["viewAlt"] = false;
+	}
   	response.render('index', projects);
 };
